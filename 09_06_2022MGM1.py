6fd3ebaad8c911536f8c5c9448b739fc2cdc6427
apurva11m <107567953+apurva11m@users.noreply.github.com>
2022-06-15 21:05:47 +0530

    Add files via upload

---
+++ 09_06_2022MGM1.py
@@ -0,0 +1,42 @@
+# # print("Hello world")
+
+# a = 5-2
+# print(f"Subtraction of two numbers {a}")
+
+#Data Types
+#1.Numeric Data types
+#int, float, complex
+# a = 25
+# b = -125
+# c = 2.50
+# d = -125.2554
+# e = 2.0
+# f = 25e9
+# g = 6j
+# h = 2+7j
+# i = "Hello"
+
+#Casting
+a = float(25)
+b = complex(-125)
+c = int(2.50)
+d = complex(-125.2554)
+e = str(2.0)
+f = int(25e9)
+g = int("6")
+# h = 2+7j
+# i = float("Hello")
+
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+# print(type(h))
+# print(type(i))
+
+print(a)
